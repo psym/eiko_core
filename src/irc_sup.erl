79aa0f29050146ea20661213ca3032ab4f5f8443
psym <psym@visualhaze.org>
2012-04-24 20:00:45 -0500

    correct supervisor type

--- src/irc_sup.erl
+++ src/irc_sup.erl
@@ -18,15 +18,14 @@ init(_) ->
 
     Restart = permanent,
     Shutdown = 2000,
-    Type = worker,
 
     Children = [
         {irc_log, {irc_log, start_link, []},
-            Restart, Shutdown, Type, [irc_log]},
+            Restart, Shutdown, worker, [irc_log]},
         {irc_config, {irc_config, start_link, ["priv/settings.cfg"]}, 
-            Restart, Shutdown, Type, [irc_config]},
+            Restart, Shutdown, worker, [irc_config]},
         {irc_network_sup, {irc_network_sup, start_link, [ok]},
-            Restart, Shutdown, Type, [irc_network_sup]}
+            Restart, Shutdown, supervisor, [irc_network_sup]}
     ],
     {ok, {SupFlags, Children}}.
 
