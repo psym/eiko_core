7fd789aa66224baf53056405d6663d29acc3878f
psym <psym@visualhaze.org>
2012-05-03 21:11:17 -0500

    fix spelling mistake

--- src/eiko_lib.erl
+++ src/eiko_lib.erl
@@ -5,25 +5,28 @@
 %%% API
 -export([ reply/3
         , parse/1
-        , get_orgin/1
+        , get_origin/1
         , get_sender/2
     ]).
 
 reply(Irc, Msg, Response) ->
-    eiko_network:send(Irc, ["PRIVMSG", " ", get_orgin(Msg)], Response).
+    eiko_network:send(Irc, ["PRIVMSG", " ", get_origin(Msg)], Response).
 
--spec get_orgin(#irc_message{}) -> binary().
-get_orgin(#irc_message{params=[<<"#", Channel/binary>>|_]} = _Msg) ->
+-spec get_origin(#irc_message{}) -> binary().
+get_origin(#irc_message{params=[<<"#", Channel/binary>>|_]} = _Msg) ->
     <<"#", Channel/binary>>;
-get_orgin(Msg) ->
+get_origin(Msg) ->
     get_sender(Msg, [nick]).
 
+%%% a user irc prefix has the form "nick!user@host"
+%%% Args is a list containing any combination of nick, user, host
+%%% a server prefix will only ever have a host
 get_sender(#irc_message{prefix = Prefix}, Args) ->
     get_sender(Prefix, Args);
 get_sender(Prefix, Args) ->
     case re:split(Prefix, "!(.*)@") of
         [Nick, Name, Host] ->
-            P = [{nick, Nick}, {name, Name}, {host, Host}];
+            P = [{nick, Nick}, {user, Name}, {host, Host}];
         [Host] ->
             P = [{host, Host}]
     end,
