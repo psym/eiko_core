42a5872308ad7da29e329cdeee320b1d421568df
psym <psym@visualhaze.org>
2012-07-14 11:02:56 -0500

    profiling option.  formatting

--- src/eiko_core.erl
+++ src/eiko_core.erl
@@ -2,19 +2,40 @@
 
 -behaviour(application).
 
--export([
-        start/0,
-        start/2,
-        stop/1
+-export([ start/0
+        , start/2
+        , stop/1
+        , profile_output/0
     ]).
 
 start() ->
     application:start(lager),
     application:start(eiko_core).
 
+-spec start(normal | {takeover, node()} | {failover, node()}, any()) -> {ok, pid()}.
 start(_StartType, _StartArgs) ->
     lager:start(),
+    consider_profiling(),
     eiko_sup:start_link([]).
 
+-spec stop(any()) -> ok.
 stop(_State) ->
-    unimplemented.
+    ok.
+
+-spec profile_output() -> ok.
+profile_output() ->
+    eprof:stop_profiling(),
+    eprof:log("procs.profile"),
+    eprof:analyze(procs),
+    eprof:log("total.profile"),
+    eprof:analyze(total).
+
+-spec consider_profiling() -> profiling | not_profiling.
+consider_profiling() ->
+    case application:get_env(profile) of
+        {ok, true} ->
+            {ok, _Pid} = eprof:start(),
+            eprof:start_profiling([self()]);
+        _ ->
+            not_profiling
+    end.
